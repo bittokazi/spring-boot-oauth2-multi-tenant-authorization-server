092abeb34d05d9d245843e83a85cef1286868499
bittokazi <bitto.kazi@gmail.com>
2025-05-13 17:54:24 +0200

    feat(mfa): MFA trusted device user agent, ip, UI login time updates ✨

--- spring-boot-oauth2-auth-server/src/main/kotlin/com/bittokazi/oauth2/auth/server/utils/HttpReqRespUtils.kt
+++ spring-boot-oauth2-auth-server/src/main/kotlin/com/bittokazi/oauth2/auth/server/utils/HttpReqRespUtils.kt
@@ -1,5 +1,6 @@
 package com.bittokazi.oauth2.auth.server.utils
 
+import eu.bitwalker.useragentutils.UserAgent
 import jakarta.servlet.http.HttpServletRequest
 
 object HttpReqRespUtils {
@@ -7,20 +8,28 @@ object HttpReqRespUtils {
     val logger = logger()
 
     private val IP_HEADER_CANDIDATES = arrayOf(
-        "X-Forwarded-For", "Proxy-Client-IP", "WL-Proxy-Client-IP",
-        "HTTP_X_FORWARDED_FOR", "HTTP_X_FORWARDED", "HTTP_X_CLUSTER_CLIENT_IP", "HTTP_CLIENT_IP",
-        "HTTP_FORWARDED_FOR", "HTTP_FORWARDED", "HTTP_VIA", "REMOTE_ADDR"
+        "X-Forwarded-For",
+        "Proxy-Client-IP",
+        "WL-Proxy-Client-IP",
+        "HTTP_X_FORWARDED_FOR",
+        "HTTP_X_FORWARDED",
+        "HTTP_X_CLUSTER_CLIENT_IP",
+        "HTTP_CLIENT_IP",
+        "HTTP_FORWARDED_FOR",
+        "HTTP_FORWARDED",
+        "HTTP_VIA",
+        "REMOTE_ADDR"
     )
 
     fun getClientIpAddressIfServletRequestExist(httpServletRequest: HttpServletRequest): String {
-        for (header in IP_HEADER_CANDIDATES) {
+        IP_HEADER_CANDIDATES.forEach { header ->
             val ipList = httpServletRequest.getHeader(header)
-            if (ipList != null && ipList.isNullOrEmpty() && !"unknown".equals(ipList, ignoreCase = true)) {
+            if (ipList != null && ipList.isNotEmpty() && !"unknown".equals(ipList, ignoreCase = true)) {
                 try {
-                    val ip = ipList.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
-                    return ip
+                    val ips = ipList.split(",").map { it.trim() }
+                    return ips.firstOrNull() ?: httpServletRequest.remoteAddr
                 } catch (ex: Exception) {
-                    logger.error("Error IP can not be extracted. ", ex.printStackTrace())
+                    logger.error("Error extracting IP address from header $header: ", ex)
                 }
             }
         }
@@ -28,7 +37,16 @@ object HttpReqRespUtils {
     }
 
     fun getUserAgent(httpServletRequest: HttpServletRequest): String {
-        return httpServletRequest.getHeader("User-Agent")
+        val userAgentString = httpServletRequest.getHeader("User-Agent")
+        return when (userAgentString.isNotEmpty()) {
+            true -> {
+                val userAgent = UserAgent.parseUserAgentString(userAgentString)
+                val os = userAgent.operatingSystem
+                val browser = userAgent.browser
+
+                "OS: ${os.name}, Browser: ${browser.name}"
+            }
+            false -> "Unknown User-Agent"
+        }
     }
 }
-
