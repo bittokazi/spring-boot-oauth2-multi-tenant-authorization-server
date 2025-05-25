265249b8a2db2cc0a08ba368fdceac510dbbe56c
Bitto Kazi <bitto.kazi@gmail.com>
2025-05-25 15:57:13 +0200

    fix(certificate): fix default cert creation :bug: (#44)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,11 @@ if [ -f "/certs/default_authkit_private_key.pem" ]; then
     echo "default cert file found, will not generate certificate."
 else
     echo "default cert file is not found, will generate default certificates."
+    cd /certs
     openssl genrsa -out default_authkit_private_key.pem 4096
     openssl rsa -pubout -in default_authkit_private_key.pem -out default_authkit_public_key.pem
     openssl pkcs8 -topk8 -in default_authkit_private_key.pem -inform pem -out default_authkit_private_key_pkcs8.pem -outform pem -nocrypt
+    cd /
 fi
 
 java -jar /app/frontend-server.jar &
