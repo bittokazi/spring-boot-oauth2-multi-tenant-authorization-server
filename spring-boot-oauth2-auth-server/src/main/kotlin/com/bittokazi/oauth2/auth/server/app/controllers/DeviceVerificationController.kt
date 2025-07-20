815b9d3a5d09657a5243f0b17ab4f6a85654454b
bittokazi <bitto.kazi@gmail.com>
2025-07-20 15:06:56 +0200

    fix(device-authorization): Support device authorization ✨

--- spring-boot-oauth2-auth-server/src/main/kotlin/com/bittokazi/oauth2/auth/server/app/controllers/DeviceVerificationController.kt
+++ spring-boot-oauth2-auth-server/src/main/kotlin/com/bittokazi/oauth2/auth/server/app/controllers/DeviceVerificationController.kt
@@ -1,29 +1,19 @@
 package com.bittokazi.oauth2.auth.server.app.controllers
 
+import com.bittokazi.oauth2.auth.server.app.services.login.DeviceVerificationService
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.oauth2.core.OAuth2UserCode
-import org.springframework.security.oauth2.server.authorization.OAuth2Authorization
-import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService
-import org.springframework.security.oauth2.server.authorization.OAuth2TokenType
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import java.lang.reflect.Field
-import java.security.Principal
 
 @Controller
 @RequestMapping("/device-verification")
 class DeviceVerificationController(
-    private val authorizationService: OAuth2AuthorizationService,
-    private val registeredClientRepository: RegisteredClientRepository
+    private val deviceVerificationService: DeviceVerificationService
 ) {
 
     @GetMapping
@@ -32,43 +22,12 @@ class DeviceVerificationController(
         model: Model,
         httpServletRequest: HttpServletRequest,
         httpServletResponse: HttpServletResponse
-    ): String {
-        if (httpServletRequest.userPrincipal == null) {
-            httpServletRequest.session.setAttribute("device_verification_redirect", true)
-            httpServletResponse.sendRedirect("/login")
-            return "device-verification"
-        }
-
-        if (userCode == null) {
-            model.addAllAttributes(mapOf(
-                "userCode" to "",
-            ))
-            return "device-verification"
-        }
-
-        val authorization = authorizationService.findByToken(
-            userCode,
-            OAuth2TokenType(USER_CODE_PARAMETER_NAME)
-        )
-
-        if (userCode != null && authorization == null) {
-            model.addAttribute("message", "Invalid or expired user code.")
-            return "device-verification-result"
-        }
-
-        // Get client ID from authorization if not provided
-        val resolvedClientId = authorization?.registeredClientId
-
-        // Get default scopes if not provided
-        val resolvedScopes = authorization?.authorizedScopes?.joinToString(" ")
-
-        model.addAllAttributes(mapOf(
-            "clientId" to resolvedClientId,
-            "userCode" to userCode,
-            "scopes" to (resolvedScopes ?: "")
-        ))
-        return "device-verification"
-    }
+    ): Any = deviceVerificationService.getDeviceVerificationPage(
+        userCode = userCode,
+        model = model,
+        httpServletRequest = httpServletRequest,
+        httpServletResponse = httpServletResponse
+    )
 
     @PostMapping()
     fun verifyUserCode(
@@ -76,79 +35,10 @@ class DeviceVerificationController(
         model: Model,
         httpServletRequest: HttpServletRequest,
         httpServletResponse: HttpServletResponse
-    ): String {
-        if (httpServletRequest.userPrincipal == null) {
-            httpServletRequest.session.setAttribute("device_verification_redirect", true)
-            httpServletResponse.sendRedirect("/login")
-            return "device-verification"
-        }
-
-        val authorization = authorizationService.findByToken(userCode, OAuth2TokenType(USER_CODE_PARAMETER_NAME))
-
-        if (authorization == null ||
-            authorization?.getToken(OAuth2UserCode::class.java)?.metadata?.get(OAuth2Authorization.Token.INVALIDATED_METADATA_NAME) as? Boolean ?: false) {
-            model.addAttribute("message", "Invalid or expired user code.")
-            return "device-verification-result"
-        }
-
-        val userAuthentication: Authentication = UsernamePasswordAuthenticationToken(
-            httpServletRequest.userPrincipal,
-            null,
-            listOf(SimpleGrantedAuthority("ROLE_USER"))
-        )
-
-        // Build an access token to mark approval
-
-        val authorizationBuilder = OAuth2Authorization.from(authorization)
-
-        val userCode = authorization.getToken(OAuth2UserCode::class.java)?.token
-
-        if (userCode == null) {
-            model.addAttribute("message", "User code not found.")
-            return "device-verification-result"
-        }
-
-        val registeredClient = registeredClientRepository.findById(authorization.registeredClientId)
-
-        if (registeredClient == null) {
-            model.addAttribute("message", "Invalid Client.")
-            return "device-verification-result"
-        }
-
-        val updatedAuthorization = authorizationBuilder
-            .principalName(httpServletRequest.userPrincipal.name)
-            .attribute(Principal::class.java.name, userAuthentication)
-            .token(userCode) { metadata ->
-                metadata[OAuth2Authorization.Token.INVALIDATED_METADATA_NAME] = true
-            }
-            .authorizedScopes(registeredClient.scopes) // ✅ tells Spring it's approved
-            .build()
-
-        authorizationService.save(updatedAuthorization)
-
-        //authorizationService.save(updated)
-
-        model.addAttribute("message", "Successful")
-
-        return "device-verification-result"
-    }
-
-    companion object {
-        const val USER_CODE_PARAMETER_NAME = "user_code"
-    }
-
-    fun findUserCodeToken(authorization: OAuth2Authorization): OAuth2Authorization.Token<*>? {
-        val field: Field = OAuth2Authorization::class.java.getDeclaredField("tokens")
-        field.isAccessible = true
-        val tokens = field.get(authorization) as Map<*, *>
-
-        return tokens.values
-            .filterIsInstance<OAuth2Authorization.Token<*>>()
-            .firstOrNull { token ->
-                val metadata = token::class.java.getDeclaredField("metadata")
-                metadata.isAccessible = true
-                val map = metadata.get(token) as? Map<*, *>
-                map?.get("token_type") == "user_code"
-            }
-    }
+    ): Any = deviceVerificationService.authorizeDevice(
+        userCode = userCode,
+        model = model,
+        httpServletRequest = httpServletRequest,
+        httpServletResponse = httpServletResponse
+    )
 }
